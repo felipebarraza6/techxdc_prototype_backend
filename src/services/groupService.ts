c7bffddac7152f06e541cdf0e9a3e65f7a04a913
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2025-06-08 13:40:08 -0500

    feat: create group service

---
+++ src/services/groupService.ts
@@ -0,0 +1,35 @@
+import Group from '../models/Group';
+
+export const GroupService = {
+    getAllGroups: async () => {
+       return await Group.findAll();
+    },
+
+    getGroupById: async (id: number) => {
+        return await Group.findByPk(id);
+    },
+    createGroup: async (groupData: {
+        name: string;
+        description?: string;
+    }) => {
+        return await Group.create(groupData);
+    },
+    updateGroup: async (id: number, groupData: {
+        name?: string;
+        description?: string;
+    }) => {
+        const group = await Group.findByPk(id);
+        if (!group) {
+            throw new Error('No existe un grupo con este ID');
+        }
+        return await group.update(groupData);
+    },
+    deleteGroup: async (id: number) => {
+        const group = await Group.findByPk(id);
+        if (!group) {
+            throw new Error('No existe un grupo con este ID');
+        }
+        await group.destroy();
+        return { message: 'Grupo eliminado correctamente' };
+    }
+}
