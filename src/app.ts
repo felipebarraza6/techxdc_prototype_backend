4156fd644b0781a23f4438abe46ad09437a635cc
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2025-06-08 14:05:02 -0500

    feat: create group routes and add into app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import exampleRoutes from './routes/exampleRoutes';
 import sequelize from './config/database';
 import userRouter from './routes/userRoute';
+import groupRouter from './routes/groupRoute';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 // Rutas
 app.use('/api/examples', exampleRoutes);
 app.use("/api/users", userRouter);
+app.use('/api/groups', groupRouter);
 
 // Ruta de prueba
 app.get('/', (_req, res) => {
