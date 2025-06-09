d751a0ac43c2b5d2c92bfcbc66221b1636bf16a5
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2025-06-09 16:55:23 -0500

    feat: add file routes and implement in app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import exampleRoutes from './routes/exampleRoutes';
 import sequelize from './config/database';
 import userRouter from './routes/userRoute';
+import fileRouter from './routes/fileRoute';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 // Rutas
 app.use('/api/examples', exampleRoutes);
 app.use("/api/users", userRouter);
+app.use('/api/files', fileRouter);
 
 // Ruta de prueba
 app.get('/', (_req, res) => {
