3e1c4a7eee0d28f67e5ff7e443056ed232aa79b7
Jimena <jimenaespinoza@gmail.com>
2025-06-12 18:28:40 -0400

    Agregando TicketType en carpeta types

---
+++ src/types/TicketType.ts
@@ -0,0 +1,24 @@
+export enum TicketPriority {
+  LOW = "low",
+  MEDIUM = "medium",
+  HIGH = "high",
+}
+
+export interface CreateTicketRequest {
+  title: string;
+  description: string;
+  created_by: number;
+  client_id: number;
+  designated: number;
+  priority: TicketPriority;
+  custom_fields?: object;
+}
+export interface UpdateTicketRequest {
+  title?: string;
+  description?: string;
+  created_by?: number;
+  client_id?: number;
+  designated?: number;
+  priority?: TicketPriority;
+  custom_fields?: object;
+}
